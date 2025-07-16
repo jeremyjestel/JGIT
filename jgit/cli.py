028295b45b302175c101b4312a724d4b7aeb70fa
jeremy jestel <jjeste8@wgu.edu>
2025-07-16 01:24:06 -0500

    add hashing and reading of files from hash

--- jgit/cli.py
+++ jgit/cli.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-
+import sys
 from . import data
 
 def main():
@@ -20,6 +20,9 @@ def parse_args():
     hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument('file')
 
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
 
     return parser.parse_args()
 
@@ -30,3 +33,7 @@ def init(args):
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
